3076a40e9b320c352828a8fa9ffae6c97da0c272
Caleb Holm <calebcholm@gmail.com>
2022-08-19 17:40:06 -0500

    Changed base url to localhost:5000

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
-  // process.env.REACT_APP_API_BASE_URL ||
-  'https://frozen-temple-19090.herokuapp.com/';
+   process.env.REACT_APP_API_BASE_URL ||
+  'http://localhost:5000';
 
 /**
  * Defines the default headers for these functions to work with `json-server`
