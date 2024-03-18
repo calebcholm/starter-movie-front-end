8e629637660443ab500f8a059d8edfff84f47cc6
Caleb Holm <calebcholm@gmail.com>
2024-03-17 20:31:08 -0500

    api_base_url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   process.env.REACT_APP_API_BASE_URL ||
-  'postgres://snnupumt:Ph5_VVhjghpbR0w1icE3BHa6rCCTyOMA@hansken.db.elephantsql.com/snnupumt';
+  'https://starter-movie-front-end-8i0l.onrender.com/';
 
 /**
  * Defines the default headers for these functions to work with `json-server`
